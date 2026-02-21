9bc0993890ac94b8e4f3723a5741a021532758ba
Goutham Kumar <goutham4kumar24@gmail.com>
2026-02-21 19:52:28 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-dev-microservices"
+    resource_group_name  = "rg-dev-microservice"
     storage_account_name = "terraformstatefilelock"
     container_name       = "statefile"
     key                  = "dev/terraform.tfstate"
   }
+
 }
