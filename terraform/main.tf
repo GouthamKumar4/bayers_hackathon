556c73f32b1cf9aa09e5de0afe5b8c0d8e875da6
ashakdt4-cpu <ashakdt4@gmail.com>
2026-02-21 16:05:06 +0000

    non error

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,12 +4,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.80",
+      version = "~> 3.80"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.6",
+      version = "~> 3.6"
     }
   }
 }
