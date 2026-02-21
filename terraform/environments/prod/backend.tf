0e44bdb50ca3be90439ea4fcd505a2f7f3e9681e
Goutham Kumar <goutham4kumar24@gmail.com>
2026-02-21 16:16:40 +0530

    backend

--- terraform/environments/prod/backend.tf
+++ terraform/environments/prod/backend.tf
@@ -1,3 +1,8 @@
 terraform {
-  backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name  = "rg-dev-microservices"
+    storage_account_name = "terraformstatefilelock"
+    container_name       = "statefile"
+    key                  = "prod/terraform.tfstate"
+  }
 }
